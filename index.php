dd90f1e40126fca8bf1789789826aecd38a6c169
raffaele-catalano <raf.catalano@gmail.com>
2023-05-22 20:01:10 +0200

    minor changes

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $moviesList = [
     new Movie('The Exorcism of Emily Rose',  'Horror',   119, 2005, 4.4),
     new Movie('Mad Max - Fury Road',         'Action',   120, 2015, 7.5, new CategoryBonus('Thriller')),
     new Movie('Shawshank Redemption',        'Dramatic', 142, 1994, 9.5),
-    new Movie('Inception',                   'Action',   148, 2010, 7.5, new CategoryBonus('Thriller')),
+    new Movie('Inception',                   'Sci-Fi',   148, 2010, 7.5, new CategoryBonus('Action')),
 ];
 
 
